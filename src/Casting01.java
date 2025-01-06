191c04f95d457f3c50e712259bbb8995f7dd5a25
Brownie124 <121064336+Brownie124@users.noreply.github.com>
2025-01-06 13:31:58 -0500

    casting 02
    not finshed

--- src/Casting01.java
+++ src/Casting01.java
@@ -1,19 +1,26 @@
 public class Casting01 {
     public static void main(String[] args) {
         //Declare and initialize a byte with a value of 128
+        byte myByte = (byte) 128;
         //Observe NetBeans' complaint
 
         
         //Declare and initialize a short with a value of 128
+        short myShort = 128;
         //Create a print statement that casts this short to a byte
-
+           byte casted = (byte) myShort;
+            System.out.println("Casted btye from short" + casted);
         
         
         //Declare and initialize a byte with a value of 127
+        byte myByte1 = (byte) 127;
         //Add 1 to this variable and print it
-        //Add 1 to this variable again and print it again
+        myByte1++;
+        System.out.println("Adding 1 to byte" + myByte1);
 
-        
+        //Add 1 to this variable again and print it again
+         myByte1++;
+        System.out.println("Adding 1 again to byte" + myByte1);
         
         
     }    
--- src/Casting02.java
+++ src/Casting02.java
@@ -1,8 +1,8 @@
 public class Casting02 {
     public static void main(String[] args) {
                 
-        //World population today
-        long currentWorldPop = 7_000_000_000;
+      //  World population today
+        long currentWorldPop = 7_000_000_000L;
         System.out.println("Current World Population: " +currentWorldPop);
         
         //World population in 1950,  Source: United Nations Population Division
@@ -15,10 +15,10 @@ public class Casting02 {
         System.out.println("World Population in 1950: " 
                 +africaPop +asiaPop +europePop +americasPop +oceanaPop);
         
-        //The current population of Asia is an estimated 60% of the world population
+       // The current population of Asia is an estimated 60% of the world population
         double percentAsia = 0.6;
-        long currentAsiaPop = currentWorldPop * percentAsia;
-        System.out.println("Current Asia Population: " +currentAsiaPop);      
+        long currentAsiaPop = (long) (currentWorldPop * percentAsia );
+        System.out.println("Current Asia Population: " +currentAsiaPop);
        
     }  
 }
