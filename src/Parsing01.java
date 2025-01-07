3744c8ea4e4f0e22cb6ec6f51cc416301a993e50
Brownie124 <121064336+Brownie124@users.noreply.github.com>
2025-01-07 17:01:01 -0500

    almost done
    need to fix error and then im finshed

--- src/Parsing01.java
+++ src/Parsing01.java
@@ -2,19 +2,23 @@ public class Parsing01 {
     public static void main(String[] args) {
         //Declare and intitialize 3 Strings: shirtPrice, taxRate, and gibberish
         String shirtPrice = "10";
-        String taxrate = "0.5";
+        String taxRate = "0.5";
         String gibberish = "ANDNQKJOWP";
         
         
         
         //Parse shirtPrice and taxRate, and print the total tax
-        
-        
+        int price = Integer.parseInt(shirtPrice);
+        double tax = Double.parseDouble(taxRate);
+        double totalTax = price * tax;
+        System.out.println("Total tax: " + totalTax);
         
         //Try to parse taxRate as an int
-        
+        int parsed = Integer.parseInt(taxRate);
+        System.out.println(parsed);
         //Try to parse gibberish as an int
-        
+        int gib = Integer.parseInt(gibberish);
+        System.out.println(gib);
     }
     
 }
