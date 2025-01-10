333521d7f08dbb07527336cfd4496ec807caa5ea
Brownie124 <121064336+Brownie124@users.noreply.github.com>
2025-01-10 15:47:18 -0500

    Finshed
    finshed parsing 01 the error still there but imm moving on

--- src/Parsing01.java
+++ src/Parsing01.java
@@ -2,20 +2,21 @@ public class Parsing01 {
     public static void main(String[] args) {
         //Declare and intitialize 3 Strings: shirtPrice, taxRate, and gibberish
         String shirtPrice = "10";
-        String taxRate = "0.5";
+         String taxRate = "0.5";
         String gibberish = "ANDNQKJOWP";
         
         
         
         //Parse shirtPrice and taxRate, and print the total tax
-        int price = Integer.parseInt(shirtPrice);
-        double tax = Double.parseDouble(taxRate);
-        double totalTax = price * tax;
-        System.out.println("Total tax: " + totalTax);
+         int price = Integer.parseInt(shirtPrice);
+         double tax = Double.parseDouble(taxRate);
+           double totaltax = tax * price;
+
+        System.out.println("Total tax: "  + totaltax);
         
         //Try to parse taxRate as an int
-        int parsed = Integer.parseInt(taxRate);
-        System.out.println(parsed);
+       int parse = Integer.parseInt(taxRate);
+        System.out.println(parse);
         //Try to parse gibberish as an int
         int gib = Integer.parseInt(gibberish);
         System.out.println(gib);
